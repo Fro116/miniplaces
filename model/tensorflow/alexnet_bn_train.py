3d0d2eb2bd05567952241c2b6831f0441b83d10f
Kundan Chintamaneni <omnik116@gmail.com>
2017-10-28 19:56:20 -0400

    Implemented Xception architecture.

--- model/tensorflow/alexnet_bn_train.py
+++ model/tensorflow/alexnet_bn_train.py
@@ -5,11 +5,12 @@ from tensorflow.contrib.layers.python.layers import batch_norm
 from DataLoader import *
 
 # Dataset Parameters
-batch_size = 256
+batch_size = 64
 load_size = 128
-fine_size = 128
+fine_size = 112
 c = 3
 data_mean = np.load("./training_mean.npy")
+experiment = 'Xception'
 
 # Training Parameters
 learning_rate = 0.001
@@ -19,7 +20,8 @@ step_display = 50
 step_save = 5000
 path_save = './models/alexnet_bn'
 start_from = ''
-regularization_scale = 0.
+regularization_scale = 0.00001
+regularizer = tf.contrib.layers.l2_regularizer(regularization_scale);
 
 def batch_norm_layer(x, train_phase, scope_bn):
     return batch_norm(x, decay=0.9, center=True, scale=True,
@@ -28,70 +30,74 @@ def batch_norm_layer(x, train_phase, scope_bn):
     reuse=None,
     trainable=True,
     scope=scope_bn)
-    
+
+def up_sample(x, keep_dropout, train_phase, name):
+    with tf.name_scope("Upwnsample_"+name):    
+        depth = x.get_shape().as_list()[3]
+        up = tf.nn.separable_conv2d(x, tf.get_variable('wud' + name, shape = [3, 3, depth, 1], initializer = tf.contrib.layers.xavier_initializer(), regularizer = regularizer),
+                                    tf.get_variable('wup' + name, shape = [1, 1, depth, depth*2], initializer = tf.contrib.layers.xavier_initializer(), regularizer = regularizer),
+                                    padding = 'SAME', strides = [1,1,1,1])
+        up = batch_norm_layer(up, train_phase, 'bu' + name)
+        up = tf.nn.relu(up)
+        return up
+
+def down_sample(x, keep_dropout, train_phase, name):
+    with tf.name_scope("Downsample_"+name):
+        depth = x.get_shape().as_list()[3]
+        dl = tf.nn.conv2d(x, tf.get_variable('wdl' + name, shape = [3, 3, depth, depth*2], initializer = tf.contrib.layers.xavier_initializer(), regularizer = regularizer),
+                          padding = 'SAME', strides = [1,2,2,1])
+        dl = batch_norm_layer(dl, train_phase, 'bdl'+ name)
+        dl = tf.nn.relu(dl)
+        
+        x = tf.nn.relu(x)
+        dr = tf.nn.separable_conv2d(x, tf.get_variable('wdrdf' + name, shape = [3, 3, depth, 1], initializer = tf.contrib.layers.xavier_initializer(), regularizer = regularizer),
+                                    tf.get_variable('wdrpf' + name, shape = [1, 1, depth, depth*2], initializer = tf.contrib.layers.xavier_initializer(), regularizer = regularizer),
+                                    padding = 'SAME', strides = [1,1,1,1])
+        dr = batch_norm_layer(dr, train_phase, 'bdrf'+ name)
+        dr = tf.nn.relu(dr)
+        dr = tf.nn.separable_conv2d(dr, tf.get_variable('wdrds' + name, shape = [3, 3, depth*2, 1], initializer = tf.contrib.layers.xavier_initializer(), regularizer = regularizer),
+                                    tf.get_variable('wdrps' + name, shape = [1, 1, depth*2, depth*2], initializer = tf.contrib.layers.xavier_initializer(), regularizer = regularizer),
+                                    padding = 'SAME', strides = [1,1,1,1])
+        dr = batch_norm_layer(dr, train_phase, 'bdrs'+ name)    
+        dr = tf.nn.max_pool(dr, ksize=[1, 3, 3, 1], strides=[1, 2, 2, 1], padding='SAME')            
+        return tf.add(dl, dr)
+
+def feature_select(x, keep_dropout, train_phase, id):
+    with tf.name_scope("FeatureSelect_" + str(id)):
+        depth = x.get_shape().as_list()[3]    
+        m = x
+        for k in range(3):
+            name = str(k) + id        
+            m = tf.nn.relu(m)
+            m = tf.nn.separable_conv2d(m, tf.get_variable('mdf' + name, shape = [3, 3, depth, 1], initializer = tf.contrib.layers.xavier_initializer(), regularizer = regularizer),
+                                       tf.get_variable('mpf' + name, shape = [1, 1, depth, depth], initializer = tf.contrib.layers.xavier_initializer(), regularizer = regularizer),
+                                       padding = 'SAME', strides = [1,1,1,1])    
+            m = batch_norm_layer(m, train_phase, 'bmf'+ name)        
+        return tf.add(x, m)
+
 def alexnet(x, keep_dropout, train_phase):
-    regularizer = tf.contrib.layers.l2_regularizer(regularization_scale)    
-    weights = {
-        'wc1': tf.get_variable('wc1', initializer = tf.random_normal([11, 11, 3, 96], stddev=np.sqrt(2./(11*11*3))), regularizer = regularizer),
-        'wc2': tf.get_variable('wc2', initializer = tf.random_normal([5, 5, 96, 256], stddev=np.sqrt(2./(5*5*96))), regularizer = regularizer),
-        'wc3': tf.get_variable('wc3', initializer = tf.random_normal([3, 3, 256, 384], stddev=np.sqrt(2./(3*3*256))), regularizer = regularizer),
-        'wc4': tf.get_variable('wc4', initializer = tf.random_normal([3, 3, 384, 256], stddev=np.sqrt(2./(3*3*384))), regularizer = regularizer),
-        'wc5': tf.get_variable('wc5', initializer = tf.random_normal([3, 3, 256, 256], stddev=np.sqrt(2./(3*3*256))), regularizer = regularizer),
+    with tf.name_scope("EntryFlow"):
+        e1 = tf.nn.conv2d(x, tf.get_variable('we1', shape = [3, 3, 3, 32], initializer = tf.contrib.layers.xavier_initializer(), regularizer = regularizer), padding = 'SAME', strides = [1,2,2,1])
+        e1 = batch_norm_layer(e1, train_phase, 'be1')
+        e1 = tf.nn.relu(e1)
 
-        'wf6': tf.get_variable('wf6', initializer = tf.random_normal([4*4*256, 4096], stddev=np.sqrt(2./(7*7*256))), regularizer = regularizer),
-        'wf7': tf.get_variable('wf7', initializer = tf.random_normal([4096, 4096], stddev=np.sqrt(2./4096)), regularizer = regularizer),
-        'wo' : tf.get_variable('wf8', initializer = tf.random_normal([4096, 100], stddev=np.sqrt(2./4096)), regularizer = regularizer)
-    }
+        e2 = tf.nn.conv2d(e1, tf.get_variable('we2', shape = [3, 3, 32, 64], initializer = tf.contrib.layers.xavier_initializer(), regularizer = regularizer), padding = 'SAME', strides = [1,1,1,1])
+        e2 = batch_norm_layer(e2, train_phase, 'be2')
+        e2 = tf.nn.relu(e2)
 
-    biases = {
-        'bo': tf.get_variable('bo', initializer = tf.ones(100))
-    }
+        e3 = down_sample(e2, keep_dropout, train_phase, str(3))
+        e4 = down_sample(e3, keep_dropout, train_phase, str(4))
 
-    # Conv + ReLU + Pool, 224->55->27
-    conv1 = tf.nn.conv2d(x, weights['wc1'], strides=[1, 4, 4, 1], padding='SAME')
-    conv1 = batch_norm_layer(conv1, train_phase, 'bn1')
-    conv1 = tf.nn.relu(conv1)
-    pool1 = tf.nn.max_pool(conv1, ksize=[1, 3, 3, 1], strides=[1, 2, 2, 1], padding='SAME')
+    with tf.name_scope("MiddleFlow"):
+        for k in range(8):
+            e4 = feature_select(e4, keep_dropout, train_phase, str(k))
 
-    # Conv + ReLU  + Pool, 27-> 13
-    conv2 = tf.nn.conv2d(pool1, weights['wc2'], strides=[1, 1, 1, 1], padding='SAME')
-    conv2 = batch_norm_layer(conv2, train_phase, 'bn2')
-    conv2 = tf.nn.relu(conv2)
-    pool2 = tf.nn.max_pool(conv2, ksize=[1, 3, 3, 1], strides=[1, 2, 2, 1], padding='SAME')
-
-    # Conv + ReLU, 13-> 13
-    conv3 = tf.nn.conv2d(pool2, weights['wc3'], strides=[1, 1, 1, 1], padding='SAME')
-    conv3 = batch_norm_layer(conv3, train_phase, 'bn3')
-    conv3 = tf.nn.relu(conv3)
-
-    # Conv + ReLU, 13-> 13
-    conv4 = tf.nn.conv2d(conv3, weights['wc4'], strides=[1, 1, 1, 1], padding='SAME')
-    conv4 = batch_norm_layer(conv4, train_phase, 'bn4')
-    conv4 = tf.nn.relu(conv4)
-
-    # Conv + ReLU + Pool, 13->6
-    conv5 = tf.nn.conv2d(conv4, weights['wc5'], strides=[1, 1, 1, 1], padding='SAME')
-    conv5 = batch_norm_layer(conv5, train_phase, 'bn5')
-    conv5 = tf.nn.relu(conv5)
-    pool5 = tf.nn.max_pool(conv5, ksize=[1, 3, 3, 1], strides=[1, 2, 2, 1], padding='SAME')
-
-    # FC + ReLU + Dropout
-    fc6 = tf.reshape(pool5, [-1, weights['wf6'].get_shape().as_list()[0]])
-    fc6 = tf.matmul(fc6, weights['wf6'])
-    fc6 = batch_norm_layer(fc6, train_phase, 'bn6')
-    fc6 = tf.nn.relu(fc6)
-    fc6 = tf.nn.dropout(fc6, keep_dropout)
-    
-    # FC + ReLU + Dropout
-    fc7 = tf.matmul(fc6, weights['wf7'])
-    fc7 = batch_norm_layer(fc7, train_phase, 'bn7')
-    fc7 = tf.nn.relu(fc7)
-    fc7 = tf.nn.dropout(fc7, keep_dropout)
-
-    # Output FC
-    out = tf.add(tf.matmul(fc7, weights['wo']), biases['bo'])
-    
-    return out
+    with tf.name_scope("ExitFlow"):
+        e5 = down_sample(e4, keep_dropout, train_phase, str(5))
+        e6 = up_sample(e5, keep_dropout, train_phase, str(6))
+        e7 = up_sample(e6, keep_dropout, train_phase, str(7))
+        e7 = tf.reduce_mean(e7, axis = [1,2])        
+        return e7
 
 # Construct dataloader
 opt_data_train = {
@@ -129,13 +135,19 @@ logits = alexnet(x, keep_dropout, train_phase)
 
 # Define loss and optimizer
 regularization_loss = tf.reduce_sum(tf.get_collection(tf.GraphKeys.REGULARIZATION_LOSSES))
+tf.summary.scalar('Regularization Loss', regularization_loss)
 evaluation_loss = tf.reduce_mean(tf.nn.sparse_softmax_cross_entropy_with_logits(labels=y, logits=logits))
+tf.summary.scalar('Evaluation Loss', evaluation_loss)
 loss = evaluation_loss + regularization_loss
+tf.summary.scalar('Loss', loss)
 train_optimizer = tf.train.AdamOptimizer(learning_rate=learning_rate).minimize(loss)
 
 # Evaluate model
 accuracy1 = tf.reduce_mean(tf.cast(tf.nn.in_top_k(logits, y, 1), tf.float32))
 accuracy5 = tf.reduce_mean(tf.cast(tf.nn.in_top_k(logits, y, 5), tf.float32))
+tf.summary.scalar('Top 1 Accuracy', accuracy1)
+tf.summary.scalar('Top 5 Accuracy', accuracy5)
+summary = tf.summary.merge_all()
 
 # define initialization
 init = tf.global_variables_initializer()
@@ -144,10 +156,12 @@ init = tf.global_variables_initializer()
 saver = tf.train.Saver()
 
 # define summary writer
-#writer = tf.train.SummaryWriter('.', graph=tf.get_default_graph())
+train_writer = tf.summary.FileWriter('logs/' + experiment + 'train', tf.get_default_graph());
+val_writer = tf.summary.FileWriter('logs/' + experiment + 'val', tf.get_default_graph());
 
 # Launch the graph
 with tf.Session() as sess:
+    
     # Initialization
     if len(start_from)>1:
         saver.restore(sess, start_from)
@@ -164,21 +178,24 @@ with tf.Session() as sess:
             print('[%s]:' %(datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")))
 
             # Calculate batch loss and accuracy on training set
-            l, acc1, acc5, reg, eval_loss = sess.run([loss, accuracy1, accuracy5, regularization_loss, evaluation_loss], feed_dict={x: images_batch, y: labels_batch, keep_dropout: 1., train_phase: False}) 
+            l, acc1, acc5, reg, eval_loss, log = sess.run([loss, accuracy1, accuracy5, regularization_loss, evaluation_loss, summary],
+                                                          feed_dict={x: images_batch, y: labels_batch, keep_dropout: 1., train_phase: False}) 
             print("-Iter " + str(step) +
                   ", Training Loss= " + "{:.6f}".format(l) +
                   ", Accuracy Top1 = " + "{:.4f}".format(acc1) +
                   ", Top5 = " + "{:.4f}".format(acc5) +
                   ", Evaluation Loss = " + "{:.4f}".format(eval_loss) +
                   ", Regularization Loss = " + "{:.4f}".format(reg))
+            train_writer.add_summary(log, step)
 
             # Calculate batch loss and accuracy on validation set
             images_batch_val, labels_batch_val = loader_val.next_batch(batch_size)    
-            l, acc1, acc5 = sess.run([loss, accuracy1, accuracy5], feed_dict={x: images_batch_val, y: labels_batch_val, keep_dropout: 1., train_phase: False}) 
+            l, acc1, acc5, log = sess.run([loss, accuracy1, accuracy5, summary], feed_dict={x: images_batch_val, y: labels_batch_val, keep_dropout: 1., train_phase: False}) 
             print("-Iter " + str(step) + ", Validation Loss= " + \
                   "{:.6f}".format(l) + ", Accuracy Top1 = " + \
                   "{:.4f}".format(acc1) + ", Top5 = " + \
                   "{:.4f}".format(acc5))
+            val_writer.add_summary(log, step)
         
         # Run optimization op (backprop)
         sess.run(train_optimizer, feed_dict={x: images_batch, y: labels_batch, keep_dropout: dropout, train_phase: True})
@@ -189,7 +206,9 @@ with tf.Session() as sess:
         if step % step_save == 0:
             saver.save(sess, path_save, global_step=step)
             print("Model saved at Iter %d !" %(step))
-        
+
+    train_writer.close()
+    val_writer.close()
     print("Optimization Finished!")
 
 
