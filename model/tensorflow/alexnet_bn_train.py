093ac9470c73e1352102e84385a837b9eb619af1
Kundan Chintamaneni <omnik116@gmail.com>
2017-11-08 21:40:03 -0500

    Experimenting with alternate training.

--- model/tensorflow/alexnet_bn_train.py
+++ model/tensorflow/alexnet_bn_train.py
@@ -18,10 +18,10 @@ dropout = 0.5 # Dropout, probability to keep units
 training_iters = 500000
 step_display = 50
 step_save = 5000
-step_phase = 500
+step_phases = [2000]
 path_save = './models/alexnet_bn'
 start_from = ''#./models/xception/alexnet_bn-50000'
-regularization_scale = 0.00001
+regularization_scale = 0.#0.00001
 regularizer = tf.contrib.layers.l2_regularizer(regularization_scale);
 
 def batch_norm_layer(x, train_phase, scope_bn):
@@ -67,7 +67,7 @@ def feature_select(x, keep_dropout, train_phase, name):
             feature = separable_conv(feature, output_depth = depth, name = "-feature_"+str(k), relu_on_entry = True)
         return tf.add(x, feature)
 
-def alexnet(x, keep_dropout, train_phase, task):
+def gen_features(x, keep_dropout, train_phase):
     with tf.variable_scope("EntryFlow"):
         step1 = conv(x, output_depth = 32, filter_size = 3, stride = 2, relu = True, name = "-1")
         step2 = conv(step1, output_depth = 64, filter_size = 3, stride = 1, relu = True, name = "-2")        
@@ -80,6 +80,9 @@ def alexnet(x, keep_dropout, train_phase, task):
         for k in range(8):
             features = feature_select(features, keep_dropout, train_phase, "-"+str(k))
 
+    return features
+
+def domain(x, keep_dropout, train_phase):
     with tf.variable_scope("ObjectRecognition"):
         domain1 = down_sample(features, first_depth = 728, second_depth = 1024, stride = 1, keep_dropout = keep_dropout, train_phase = train_phase, name = "-1")
         domain2 = separable_conv(domain1, output_depth = 1536, name = "-2", relu_on_entry = False)
@@ -94,36 +97,42 @@ def alexnet(x, keep_dropout, train_phase, task):
         domain4 = separable_conv(domain3, output_depth = 100, name = "-4", relu_on_entry = False)
         scenes = tf.reduce_mean(domain4, axis = [1,2])
 
-    if task == 'ObjectRecognition':
-        return objects
-    else:# task == 'SceneRecognition':
-        return scenes
+    return [scenes, objects]
     
 # tf Graph input
 x = tf.placeholder(tf.float32, [None, fine_size, fine_size, c])
 y = tf.placeholder(tf.int64, None)
 keep_dropout = tf.placeholder(tf.float32)
 train_phase = tf.placeholder(tf.bool)
-task = tf.placeholder(tf.string)
-task_val = "SceneRecognition"
+tasks = ["SceneRecognition", "ObjectRecognition"]
 
-# Construct model
-logits = alexnet(x, keep_dropout, train_phase, task_val)
+domain_optimizers = [0 for i in range(len(tasks))]
+feature_optimizers = [0 for i in range(len(tasks))]
+full_optimizers = [0 for i in range(len(tasks))]
+accuracy1s = [0 for i in range(len(tasks))]
+accuracy5s = [0 for i in range(len(tasks))]
+indexes = [0 for i in range(len(tasks))]
+losses = [0 for i in range(len(tasks))]
+regularization_losses = [0 for i in range(len(tasks))]
+evaluation_losses = [0 for i in range(len(tasks))]
 
-# Define loss and optimizer
-regularization_loss = tf.reduce_sum(tf.get_collection(tf.GraphKeys.REGULARIZATION_LOSSES))
-evaluation_loss = tf.reduce_mean(tf.nn.sparse_softmax_cross_entropy_with_logits(labels=y, logits=logits))
-loss = evaluation_loss + regularization_loss
-domain_list = tf.get_collection(tf.GraphKeys.TRAINABLE_VARIABLES, scope = task_val)
-feature_list = tf.get_collection(tf.GraphKeys.GLOBAL_VARIABLES, scope = "EntryFlow") + tf.get_collection(tf.GraphKeys.GLOBAL_VARIABLES, scope = "MiddleFlow")
-domain_optimizer = tf.train.AdamOptimizer(learning_rate=learning_rate).minimize(loss, var_list = domain_list)
-feature_optimizer = tf.train.AdamOptimizer(learning_rate=learning_rate).minimize(loss, var_list = feature_list)
-full_optimizer = tf.train.AdamOptimizer(learning_rate=learning_rate).minimize(loss)
+features = gen_features(x, keep_dropout, train_phase)
+logits_arr = domain(features, keep_dropout, train_phase)
+for i in range(len(tasks)):
+    task = tasks[i]
+    logits = logits_arr[i]
+    regularization_losses[i] = tf.reduce_sum(tf.get_collection(tf.GraphKeys.REGULARIZATION_LOSSES))
+    evaluation_losses[i] = tf.reduce_mean(tf.nn.sparse_softmax_cross_entropy_with_logits(labels=y, logits=logits))
+    losses[i] = evaluation_losses[i] + regularization_losses[i]
+    domain_list = tf.get_collection(tf.GraphKeys.TRAINABLE_VARIABLES, scope = task)
+    feature_list = tf.get_collection(tf.GraphKeys.GLOBAL_VARIABLES, scope = "EntryFlow") + tf.get_collection(tf.GraphKeys.GLOBAL_VARIABLES, scope = "MiddleFlow")
+    domain_optimizers[i] = tf.train.AdamOptimizer(learning_rate=learning_rate).minimize(losses[i], var_list = domain_list)
+    feature_optimizers[i] = tf.train.AdamOptimizer(learning_rate=learning_rate).minimize(losses[i], var_list = feature_list)
+    full_optimizers[i] = tf.train.AdamOptimizer(learning_rate=learning_rate).minimize(losses[i])
 
-# Evaluate model
-values, indices = tf.nn.top_k(logits, 1)
-accuracy1 = tf.reduce_mean(tf.cast(tf.nn.in_top_k(logits, y, 1), tf.float32))
-accuracy5 = tf.reduce_mean(tf.cast(tf.nn.in_top_k(logits, y, 5), tf.float32))
+    values, indexes[i] = tf.nn.top_k(logits, 1)
+    accuracy1s[i] = tf.reduce_mean(tf.cast(tf.nn.in_top_k(logits, y, 1), tf.float32))
+    accuracy5s[i] = tf.reduce_mean(tf.cast(tf.nn.in_top_k(logits, y, 5), tf.float32))
 
 # define initialization
 init = tf.global_variables_initializer()
@@ -140,38 +149,43 @@ def initialize(sess):
 
 def train(sess):
     step = 0
-    phases = ['full']#['feature', 'domain']
-    tasks = ["SceneRecognition"]#["ObjectRecognition", "SceneRecognition"]
+    phase_schedule = ['full']#'feature', 'domain']
+    phases = {'feature' : 0, 'domain' : 1, 'full' : 2}
     loader_trains = [scene_loader_train, obj_loader_train]
     loader_vals = [scene_loader_val, obj_loader_val]
     loader_tests = [scene_loader_test, obj_loader_test]
-    optimizers = [full_optimizer, domain_optimizer, feature_optimizer]
+    optimizers = [domain_optimizers, feature_optimizers, full_optimizers]
     phase_index = 0
+    step_phase = step_phases[phase_index]
     
     while step < training_iters:
-        task_index = np.random.random_integers(0, len(tasks)-1)        
-        task_str = tasks[task_index]
+        if step % step_phase < step_phase/2:
+            task_index = 0
+        else:
+            task_index = 1
+        i = task_index
         loader_train = loader_trains[task_index]
         loader_val = loader_vals[task_index]
         images_batch, labels_batch = loader_train.next_batch(batch_size)
 
+        if step % step_phase == 0:
+            phase_index += 1
+            if phase_index == len(phase_schedule):
+                phase_index = 0
+            phase = phases[phase_schedule[phase_index]]
+            step_phase = step_phases[phase_index]
+
         if step % step_display == 0:
             print('[%s]:' %(datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")))
-            l, acc1, acc5, reg, eval_loss = sess.run([loss, accuracy1, accuracy5, regularization_loss, evaluation_loss],
-                                                     feed_dict={x: images_batch, y: labels_batch, keep_dropout: 1., train_phase: False, task: task_str}) 
-            print("-Iter " + str(step) + ", Training Loss= " + "{:.6f}".format(l) + ", Accuracy Top1 = " + "{:.4f}".format(acc1) +
+            l, acc1, acc5, reg, eval_loss = sess.run([losses[i], accuracy1s[i], accuracy5s[i], regularization_losses[i], evaluation_losses[i]],
+                                                     feed_dict={x: images_batch, y: labels_batch, keep_dropout: 1., train_phase: False}) 
+            print("-Iter " + str(step) + ", Phase " + phase_schedule[phase_index] + ", task " + tasks[task_index] +", Training Loss= " + "{:.6f}".format(l) + ", Accuracy Top1 = " + "{:.4f}".format(acc1) +
                   ", Top5 = " + "{:.4f}".format(acc5) + ", Evaluation Loss = " + "{:.4f}".format(eval_loss) + ", Regularization Loss = " + "{:.4f}".format(reg))
             images_batch_val, labels_batch_val = loader_val.next_batch(batch_size)    
-            l, acc1, acc5 = sess.run([loss, accuracy1, accuracy5], feed_dict={x: images_batch, y: labels_batch, keep_dropout: 1., train_phase: False, task: task_str}) 
+            l, acc1, acc5 = sess.run([losses[i], accuracy1s[i], accuracy5s[i]], feed_dict={x: images_batch_val, y: labels_batch_val, keep_dropout: 1., train_phase: False}) 
             print("-Iter " + str(step) + ", Validation Loss= " + "{:.6f}".format(l) + ", Accuracy Top1 = " + "{:.4f}".format(acc1) + ", Top5 = " + "{:.4f}".format(acc5))
-
-        if step % step_phase == 0:
-            phase = phases[phase_index]
-            phase_index += 1
-            if phase_index == len(phases):
-                phase_index = 0
             
-        sess.run(optimizers[phase_index], feed_dict={x: images_batch, y: labels_batch, keep_dropout: dropout, train_phase: True, task: task_str})
+        sess.run(optimizers[phase][task_index], feed_dict={x: images_batch, y: labels_batch, keep_dropout: dropout, train_phase: True})
             
         step += 1
                 
@@ -180,12 +194,12 @@ def train(sess):
             print("Model saved at Iter %d !" %(step))
             for i in range(len(tasks)):
                 print("Task:" + str(tasks[i]))
-                validate(sess, loader_vals[i], tasks[i])
-                evaluate(sess, loader_tests[i], tasks[i])
+                validate(sess, loader_vals[i], i)
+#                evaluate(sess, loader_tests[i], i)
             
     print("Optimization Finished!")
 
-def validate(sess, loader_val, task_str):
+def validate(sess, loader_val, task_index):
     # Evaluate on the whole validation set
     print('Evaluation on the whole validation set...')
     acc1_total = 0.
@@ -200,14 +214,14 @@ def validate(sess, loader_val, task_str):
             size = loader_val.size() - i
         i += size
         images_batch, labels_batch = loader_val.next_batch(size)    
-        acc1, acc5 = sess.run([accuracy1, accuracy5], feed_dict={x: images_batch, y: labels_batch, keep_dropout: 1., train_phase: False, task: task_str})
+        acc1, acc5 = sess.run([accuracy1s[task_index], accuracy5s[task_index]], feed_dict={x: images_batch, y: labels_batch, keep_dropout: 1., train_phase: False})
         acc1_total += acc1 * size
         acc5_total += acc5 * size
     acc1_total /= loader_val.size()
     acc5_total /= loader_val.size()
     print('Validation Finished! ', 'Accuracy Top1 = ' + "{:.4f}".format(acc1_total) + ", Top5 = " + "{:.4f}".format(acc5_total))
 
-def evaluate(sess, loader_test, task_str):
+def evaluate(sess, loader_test, task_index):
     print('Evaluation on the whole test set...')
     predictions = []
     i = 0
@@ -218,7 +232,7 @@ def evaluate(sess, loader_test, task_str):
             size = loader_test.size() - i
         i += size
         images_batch, labels_batch = loader_test.next_batch(size)    
-        preds = sess.run(indices, feed_dict={x: images_batch, y: labels_batch, keep_dropout: 1., train_phase: False, tasks: task_str})
+        preds = sess.run(indexes[task_index], feed_dict={x: images_batch, y: labels_batch, keep_dropout: 1., train_phase: False})
         predictions = predictions + [x[0] for x in preds]
     print(predictions)    
     
